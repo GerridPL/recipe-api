756aee1f399a1ad927e8bb6322d42477f6ae15a8
Hubert Wanga <hubert.wanga@gmail.com>
2022-11-12 11:24:32 +0100

    Fix first endpoint.

--- src/main/java/pl/gerrid/recipe/dataProvider/interfaces/IngredientDataProviderInterface.java
+++
@@ -1,9 +0,0 @@
-package pl.gerrid.recipe.dataProvider.interfaces;
-
-import pl.gerrid.recipe.model.Ingredient;
-
-import java.util.List;
-
-public interface IngredientDataProviderInterface {
-    List<Ingredient> findAll();
-}
--- src/main/java/pl/gerrid/recipe/entity/IngredientEntity.java
+++ src/main/java/pl/gerrid/recipe/entity/IngredientEntity.java
@@ -2,7 +2,6 @@ package pl.gerrid.recipe.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import pl.gerrid.recipe.model.Ingredient;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -14,8 +13,4 @@ public class IngredientEntity {
     @Id
     private int id;
     private String name;
-
-    public Ingredient toModel() {
-        return new Ingredient(this.id, this.name);
-    }
 }
