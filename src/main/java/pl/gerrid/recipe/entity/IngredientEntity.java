47853a5de23a0b4d714799ac4f4bc71baa5e8e74
Hubert Wanga <hubert.wanga@gmail.com>
2022-11-12 13:24:17 +0100

    Connect with database.

--- src/main/java/pl/gerrid/recipe/entity/IngredientEntity.java
+++ src/main/java/pl/gerrid/recipe/entity/IngredientEntity.java
@@ -3,14 +3,17 @@ package pl.gerrid.recipe.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "ingredient_entity")
 public class IngredientEntity {
     @Id
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "name")
     private String name;
 }
